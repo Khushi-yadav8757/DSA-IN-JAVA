eef6dd1a5c12a12b000236ebb3681c4989de3c61
Khushi Yadav <kky957242@gmail.com>
2025-12-15 21:56:42 +0530

    Create Allmoves.java

---
+++ Allmoves.java
@@ -0,0 +1,16 @@
+//Move All Zeros to End
+class Main {
+    static void moveZeroes(int[] arr) {
+        int index = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                arr[index++] = arr[i];
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index++] = 0;
+        }
+    }
+}
