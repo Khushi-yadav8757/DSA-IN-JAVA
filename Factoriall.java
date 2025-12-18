767e0366c3529b0b68dc6fa4acbffbfdb9d9b1b4
Khushi Yadav <kky957242@gmail.com>
2025-12-18 15:31:53 +0530

    Update Factoriall.java

--- Factoriall.java
+++ Factoriall.java
@@ -4,7 +4,6 @@ public class Factorial {
             return 1;
         return n * factorial(n - 1);
     }
-
     public static void main(String[] args) {
         int num = 5;
         System.out.println("Factorial: " + factorial(num));
