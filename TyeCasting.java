70e2e3aa1a5dd07b571a67089e384210ab510bf3
Khushi Yadav <kky957242@gmail.com>
2025-12-19 15:15:20 +0530

    Remove unnecessary comment in TyeCasting.java
    Removed comment line before output statement.

--- TyeCasting.java
+++ TyeCasting.java
@@ -3,7 +3,6 @@ public class Tester {
       int num1 = 5004;
       double num2 = 2.5;
       double sum = num1 + num2;
-      // output
       System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
    }
 }
