1d9f4d3a63aa2d59115ccbf920e28340d0ee534c
Khushi Yadav <kky957242@gmail.com>
2026-01-16 21:28:28 +0530

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,6 @@
 
 import java.util.Scanner;
 import java.util.Random;
-
 public class GuessTheNumberGame{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
