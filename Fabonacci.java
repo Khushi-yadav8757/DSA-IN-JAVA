0b56b7d6ac013547dad3a9a1f1ba93fcbd7fd7db
Khushi Yadav <kky957242@gmail.com>
2026-01-24 11:43:39 +0530

    Update Fabonacci.java

--- Fabonacci.java
+++ Fabonacci.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter number of terms: ");
         int n = sc.nextInt();
         int a=0,  b=1;
