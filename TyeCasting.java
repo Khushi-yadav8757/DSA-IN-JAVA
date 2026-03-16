98a07849976832947de3b52c7fde3c6bf66a9241
Khushi Yadav <kky957242@gmail.com>
2026-03-16 20:11:20 +0530

    Update TyeCasting.java

--- TyeCasting.java
+++ TyeCasting.java
@@ -1,7 +1,7 @@
 public class Tester {
    public static void main(String[] args) {
       int num1 = 5004;
-      double num2 = 2.5;
+      double num2 = 2.5; 
       double sum = num1 + num2;
       System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
    }
