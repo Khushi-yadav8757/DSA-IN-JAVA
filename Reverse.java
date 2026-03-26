8ee180e2c471573aa1fb0386e9d7bf5f76afe39b
Khushi Yadav <kky957242@gmail.com>
2026-03-26 15:36:47 +0530

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -6,7 +6,6 @@ public class ReverseNumber {
         System.out.print("Enter a number: ");
         int num = sc.nextInt();
         int reverse = 0;
-
         while (num != 0) {
             int digit = num % 10;
             reverse = reverse * 10 + digit;
